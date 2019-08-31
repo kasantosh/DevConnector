a0f586392e0759062868006d7510cd7f862bbffc
Santosh Kumar <kasantosh@yahoo.com>
2019-08-31 10:29:13 -0400

    Not able to save to mongoDB - troubleshooting

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -51,64 +51,68 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    // const {
-    //   company,
-    //   website,
-    //   location,
-    //   bio,
-    //   status,
-    //   githubusername,
-    //   skills,
-    //   youtube,
-    //   facebook,
-    //   twitter,
-    //   instagram,
-    //   linkedin
-    // } = req.body;
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin
+    } = req.body;
 
-    // // Build profile object
-    // const profileFields = {};
-    // profileFields.user = req.user.id;
+    // Build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
 
-    // if (company) profileFields.company = company;
-    // if (website) profileFields.website = website;
-    // if (location) profileFields.location = location;
-    // if (bio) profileFields.bio = bio;
-    // if (status) profileFields.status = status;
-    // if (githubusername) profileFields.githubusername = githubusername;
-    // if (skills)
-    //   profileFields.skills = skills.split(",").map(skill => skill.trim());
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
 
-    // // Build Social Object
-    // profileFields.social = {};
-    // if (youtube) profileFields.social.youtube = youtube;
-    // if (twitter) profileFields.social.twitter = twitter;
-    // if (facebook) profileFields.social.facebook = facebook;
-    // if (linkedin) profileFields.social.linkedin = linkedin;
-    // if (instagram) profileFields.social.instagram = instagram;
+    if (skills) {
+      profileFields.skills = skills.split(",").map(skill => skill.trim());
+    }
 
-    // try {
-    //   let profile = await Profile.findOne({ user: req.user.id });
+    // Build Social Object
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (instagram) profileFields.social.instagram = instagram;
 
-    //   if (profile) {
-    //     // Update
-    //     profile = await Profile.findOneAndUpdate(
-    //       { user: req.user.id },
-    //       { $set: profileFields },
-    //       { new: true }
-    //     );
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
 
-    //     return res.json(profile);
-    //   }
+      if (profile) {
+        // Update
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+        return res.json(profile);
+      }
 
-    //   // Create
-    //   profile = new Profile(profileFields);
-    //   await profile.save();
-    //   res.json(profile);
-    // } catch (err) {
-    //   console.error(err.message);
-    //   res.status(500).send("Server Error");
-    // }
+      // Create
+      profile = new Profile(profileFields);
+      console.log(profile);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
   }
 );
 
